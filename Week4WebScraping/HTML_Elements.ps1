544eb62ecaab7479558e8c29f7c799aea44e9f6f
Rune <rune.seregina@mymail.champlain.edu>
2025-09-25 17:54:12 -0400

    HTML elements script

---
+++ Week4WebScraping/HTML_Elements.ps1
@@ -0,0 +1,19 @@
+﻿$scraped_page = Invoke-WebRequest -TimeoutSec 10 http://10.0.17.21/ToBeScraped.html
+
+# Get count of links in page
+# $scraped_page.Links.Count
+
+# Display links as HTML element
+# $scraped_page.Links
+
+# Display only URL and its text
+# $scraped_page.Links | select href, outerText
+
+# $h2s = $scraped_page.ParsedHtml.body.getElementsByTagName("h2") | select outerText
+# $h2s
+
+# Print innerText of every div element that has the class "div-1"
+$divs1 = $scraped_page.ParsedHtml.body.getElementsByTagName("div") | where { `
+$_.getAttributeNode("class").Value -ilike "div-1" } | select innerText
+
+$divs1
