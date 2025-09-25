ae3c2fafc06f032751344e5a92e2e522a5ea4142
Rune <rune.seregina@mymail.champlain.edu>
2025-09-25 18:37:32 -0400

    parsing apache logs script added

--- Week4WebScraping/ApacheLogs/Main.ps1
+++ Week4WebScraping/ApacheLogs/Main.ps1
@@ -1,5 +1,8 @@
 ﻿. (Join-Path $PSScriptRoot "Apache-Logs.ps1")
+. (Join-Path $PSScriptRoot "Parse-Apache-Logs.ps1")
 
 clear
 
 Get-ApacheLogIPs -Page "index.html" -HTTPCode "200" -Browser "Chrome"
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
---
+++ Week4WebScraping/ApacheLogs/Parse-Apache-Logs.ps1
@@ -0,0 +1,25 @@
+﻿<# ********************************************
+Function: Format a table of log records for pages accessed by 10.x.x.x IP addresses
+    Input: None
+    Output: formatted table of records including IP, Time, Method, Page, Protocol, Response, Referrer, and Client
+*********************************************#>
+function ApacheLogs1() {
+$logsNotformatted = Get-Content C:\xampp\apache\logs\access.log
+$tableRecords = @()
+
+for($i=0; $i -lt $logsNotformatted.Count; $i++){
+
+# split a string into words
+$words = $logsNotformatted[$i].Split(" ");
+
+$tableRecords += [pscustomobject] @{ "IP" = $words[0];
+                                     "Time" = $words[3].Trim('[');
+                                     "Method" = $words[4].Trim('"');
+                                     "Page" = $words[6];
+                                     "Protocol" = $words[7];
+                                     "Response" =$words[8];
+                                     "Referrer" = $words[10];
+                                     "Client" = $words[11] }
+} # end of for loop
+return $tableRecords | Where-Object { $_.IP -ilike "10.*" }
+}
