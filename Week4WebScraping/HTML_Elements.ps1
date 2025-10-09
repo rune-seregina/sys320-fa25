733b835add175391423c6122ab5dbcdef626987a
Rune Seregina <143445394+rune-seregina@users.noreply.github.com>
2025-10-09 12:43:55 -0400

    Update HTML_Elements.ps1

--- Week4WebScraping/HTML_Elements.ps1
+++ Week4WebScraping/HTML_Elements.ps1
@@ -9,6 +9,7 @@ $scraped_page.Links
 # Display only URL and its text
 $scraped_page.Links | select href, outerText
 
+# Get outer text of every element with the tag h2
 $h2s = $scraped_page.ParsedHtml.body.getElementsByTagName("h2") | select outerText
 $h2s
 
