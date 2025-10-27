a93086dcc98567d04caad255b48103a08a3ec277
Rune Seregina <143445394+rune-seregina@users.noreply.github.com>
2025-10-27 15:29:58 -0400

    Update Email.ps1

--- ScheduledTasks/Email.ps1
+++ ScheduledTasks/Email.ps1
@@ -11,7 +11,7 @@ $To = "rune.seregina@mymail.champlain.edu"
 $Subject = "Suspicious Activity"
 
 
-$Password = "gcdh nsbl lsmk idwm" | ConvertTo-SecureString -AsPlainText -Force
+$Password = "redacted" | ConvertTo-SecureString -AsPlainText -Force
 $Credential = New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $From, $Password
 
 Send-MailMessage -From $From -To $To -Subject $Subject -Body $Body -SmtpServer "smtp.gmail.com" -port 587 -UseSsl -Credential $Credential
